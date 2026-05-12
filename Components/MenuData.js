6d738962020ce0cd311e45a7a63333b31d99a4e4
max-57 <81874292+max-57@users.noreply.github.com>
2026-05-12 12:44:22 +0400

    Регистр и кириллица

---
+++ Components/MenuData.js
@@ -0,0 +1,18 @@
+const navbarConfig = {
+    logo: {
+        src: './images/Logo.svg',
+        alt: 'Лучший продакт дизайнер',
+        link: 'main' // Изменил на ID для SPA
+    },
+    menu: [
+        { title: 'Обо мне', link: 'about-me' },
+        { title: 'Портфолио', link: 'portfolio' },
+        { title: 'Отзывы', link: 'reviews' }
+    ],
+    socials: [
+        { name: 'linkedin', link: 'https://www.linkedin.com/in/maxim-ermolov-365820227/', img: './images/Linkedin.svg' },
+        { name: 'mail', link: 'mailto:max.ermolovv@gmail.com', img: './images/mail.svg' },
+        { name: 'telegram', link: 'https://t.me/@max_ermolov', img: './images/Telegram.svg' },
+        { name: 'phone', link: 'tel:+37499192501', img: './images/Phone.svg' }
+    ]
+};
--- Main.js
+++ Main.js
@@ -1,22 +1,6 @@
-const navbarConfig = {
-    logo: {
-        src: './images/Logo.svg',
-        alt: 'Лучший продакт дизайнер',
-        link: 'main' // Изменил на ID для SPA
-    },
-    menu: [
-        { title: 'Обо мне', link: 'about-me' },
-        { title: 'Портфолио', link: 'portfolio' },
-        { title: 'Отзывы', link: 'reviews' }
-    ],
-    socials: [
-        { name: 'linkedin', link: 'https://www.linkedin.com/in/maxim-ermolov-365820227/', img: './images/Linkedin.svg' },
-        { name: 'mail', link: 'mailto:max.ermolovv@gmail.com', img: './images/mail.svg' },
-        { name: 'telegram', link: 'https://t.me/@max_ermolov', img: './images/Telegram.svg' },
-        { name: 'phone', link: 'tel:+37499192501', img: './images/Phone.svg' }
-    ]
-};
-
+//=======================================================
+//  Меню и переходы
+//=========================================================
 // --- ФУНКЦИЯ SPA НАВИГАЦИИ ---
 async function navigate(pageId) {
     const contentArea = document.querySelector('.content'); // Твой блок контента
@@ -88,7 +72,7 @@ function initNavbar() {
 
     if (menuBox) {
         // Определяем начальную страницу из URL (хэша) или ставим main
-        const initialPage = window.location.hash.replace('#', '') || 'main';
+        const initialPage = window.location.hash.replace('#', '') || 'about-me';
         
         menuBox.innerHTML = `
             ${navbarConfig.menu.map(item => `
@@ -161,12 +145,12 @@ function initSPA() {
 
     // Кнопки "Назад/Вперед" в браузере
     window.addEventListener('popstate', (e) => {
-        const pageId = window.location.hash.replace('#', '') || 'main';
+        const pageId = window.location.hash.replace('#', '') || 'about-me';
         navigate(pageId);
     });
 
     // Первая загрузка при открытии сайта
-    const initialPage = window.location.hash.replace('#', '') || 'main';
+    const initialPage = window.location.hash.replace('#', '') || 'about-me';
     navigate(initialPage);
 }
 
